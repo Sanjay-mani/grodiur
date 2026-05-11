f436fa4f1f32838e34bb03a320da2f2be6223ca7
sanjay <sanjaymsanju011@gmail.com>
2026-05-11 10:43:44 +0530

    Register Coupon and Cart models in admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Coupon, Cart, CartItem
 
-# Register your models here.
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount_type', 'amount', 'min_order_value', 'is_active', 'expiry_date')
+    list_filter = ('is_active', 'discount_type')
+    search_fields = ('code',)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'coupon', 'created_at')
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
